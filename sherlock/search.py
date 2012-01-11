05b75452edfed95f09656cc656b3fbad06a2596e
Jean-Paul Rose <jprose@monainformatixltd.com>
2012-01-11 12:13:39 -0500

    Completed basic search example. At the momement, implementation searches for 'Jamaica' and creates a report if found

--- sherlock/search.py
+++ sherlock/search.py
@@ -1,8 +1,13 @@
 from mapstream2.listener.models import RawData, DataTag
+from mapstream2.stream.models import EventReport
+from django.contrib.auth.models import User
 import json
 
 class BasicSearchAgent():
 	"""This agent does a simple search on RawData objects, tagging them as valid if it finds 'Jamaica' in them."""
+
+	def __init__(self):
+		self.sys_user = User.objects.get(username='system')
 	
 	def do_search(self):
 		"""First Pass of basic search
@@ -17,7 +22,13 @@ class BasicSearchAgent():
 				data_description = data_obj['description']
 				if 'Jamaica' in data_description:
 					valid_event_count = valid_event_count + 1
-					print 'Need to create event here!!'
+					event_report = EventReport()
+					event_report.title = 'Sys created report'
+					event_report.made_by = self.sys_user
+					event_report.confidence = 0.5
+					event_report.save()
+					print 'Created new report'
+
 			except KeyError:
 				print 'no description in raw_data #%s - %s\n' % (raw_data.id, raw_data.data)
 		print "we have %s occurences of Jamaica!" % valid_event_count
