761dc0eff5dd5f33fd548614f72ce11402602c0b
Jean-Mark Wright <jmwright@monainformatixltd.com>
2012-04-29 13:51:55 -0500

    sql community file and shp files

---
+++ sql/stream_geoobject.sql
@@ -0,0 +1,30 @@
+--
+-- PostgreSQL database dump
+--
+
+SET statement_timeout = 0;
+SET client_encoding = 'UTF8';
+SET standard_conforming_strings = off;
+SET check_function_bodies = false;
+SET client_min_messages = warning;
+SET escape_string_warning = off;
+
+SET search_path = public, pg_catalog;
+
+--
+-- Name: stream_geoobject_id_seq; Type: SEQUENCE SET; Schema: public; Owner: -
+--
+
+SELECT pg_catalog.setval('stream_geoobject_id_seq', 3, true);
+
+
+--
+-- Data for Name: stream_geoobject; Type: TABLE DATA; Schema: public; Owner: -
+--
+
+
+
+--
+-- PostgreSQL database dump complete
+--
+
